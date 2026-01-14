f7b8e4b0c6f4db5f82380272588fab38a04477fe
nickmux <sm64fan111@gmail.com>
2026-01-13 22:00:39 -0500

    Started Work on cars

---
+++ Scripts/car.gd
@@ -0,0 +1,9 @@
+extends CharacterBody2D
+
+
+const SPEED = 500.0
+const JUMP_VELOCITY = -400.0
+
+func _physics_process(delta: float) -> void:
+	velocity.x = SPEED 
+	move_and_slide()
--- Scripts/road.gd
+++ Scripts/road.gd
@@ -1,11 +1,20 @@
 extends Node2D
 
+var car = preload("res://Objects/World/Car.tscn")
+@export var marker_2d: Marker2D 
+@export var timer: Timer 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	timer.start(randi_range(1,2.3))
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
+func _on_timer_timeout() -> void:
+	var sceneCar = car.instantiate()
+	sceneCar.global_position = marker_2d.global_position 
+	add_child(sceneCar)
+	timer.start(randi_range(1,2.3))
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
